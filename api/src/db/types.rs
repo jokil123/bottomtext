415d959821a97e7fd1e63bd461d9da5169bbb686
Joshua Lung <jo.kil.plays@gmail.com>
2022-09-15 23:26:32 +0200

    working prototype for websocket

--- api/src/db/types.rs
+++ api/src/db/types.rs
@@ -1,7 +1,4 @@
-use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
-
-use super::{FRAME_DELIMITER, SUBTEXT_DELIMITER};
 
 #[derive(Serialize, Deserialize)]
 pub struct FramesJson {
