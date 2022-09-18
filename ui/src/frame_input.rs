1695d959d33c642fae7905592b33d77d8a0da88f
Joshua Lung <jo.kil.plays@gmail.com>
2022-09-18 02:33:52 +0200

    refactor

--- ui/src/frame_input.rs
+++ ui/src/frame_input.rs
@@ -1,4 +1,5 @@
 use ui::get_html_element_by_id;
+use web_sys::Event;
 use yew::{function_component, html, use_state, Html, KeyboardEvent, UseStateHandle};
 
 #[function_component(FrameInput)]
@@ -6,9 +7,15 @@ pub fn frame_input() -> Html {
     let text: UseStateHandle<String> = use_state(|| "".to_string());
     let subtext: UseStateHandle<String> = use_state(|| "".to_string());
 
+    let onkeypress_handler = |e| {
+        handle_keypress(e, || {
+            get_html_element_by_id("subtext").unwrap().focus();
+        })
+    };
+
     html! {
         <div>
-            <input id={"text"} onkeypress={|e| {handle_keypress(e, || {get_html_element_by_id("subtext").unwrap().focus();})}} placeholder={"Text"}/>
+            <input id={"text"} onchange={|e: Event| {e.target().unwrap();}} onkeypress={onkeypress_handler} placeholder={"Text"} value={(*text).clone()}/>
             <input id={"subtext"} onkeypress={|e| {handle_keypress(e, submit)}} placeholder={"Optional Subtext"}/>
         </div>
     }
