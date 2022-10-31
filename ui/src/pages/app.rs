cce774972ea35b3a31c779450ea81ee5fb905ea0
Joshua Lung <jo.kil.plays@gmail.com>
2022-10-31 10:38:51 +0100

    websocket working with yew-hooks

--- ui/src/pages/app.rs
+++ ui/src/pages/app.rs
@@ -1,73 +1,43 @@
 use clone_all::clone_all;
-use common::frame;
-use common::frame::FrameJson;
-use common::frame::FramesJson;
-use futures::SinkExt;
-use futures::StreamExt;
-use gloo_net::websocket::futures::WebSocket;
-use gloo_net::websocket::Message;
-use wasm_bindgen_futures::spawn_local;
-use yew::callback;
 use yew::prelude::*;
 use yew_agent::Bridged;
+use yew_hooks::{use_effect_once, use_web_socket};
 
 use crate::components::frame::Frame;
 use crate::components::frame_input::FrameInput;
-use crate::event_bus::EventBus;
 use crate::model::FrameModel;
-// use crate::ws::WebsocketService;
 
 #[function_component(App)]
 pub fn app() -> Html {
     let frame: UseStateHandle<FrameModel> = use_state(|| FrameModel::default());
 
+    let ws = use_web_socket("ws://localhost:8080/api/ws".to_string());
+
     {
-        clone_all!(frame);
+        clone_all!(frame, ws);
         use_effect_with_deps(
-            move |_| {
-                // let frame = frame.clone();
-                wasm_bindgen_futures::spawn_local({
-                    clone_all!(frame);
-                    async move {
-                        frame.set(FrameModel::from_request().await);
+            move |message| {
+                if let Some(message) = &**message {
+                    if let Ok(f) = serde_json::from_str(&message) {
+                        frame.set(frame.push_front(f));
                     }
-                });
+                }
                 || ()
             },
-            (),
+            ws.message,
         );
     }
 
     {
         clone_all!(frame);
-        use_effect_with_deps(
-            move |_| {
-                let ws = WebSocket::open("ws://127.0.0.1:8080/api/ws").unwrap();
-                let (write, mut read) = ws.split();
-
-                spawn_local(async move {
-                    while let Some(r) = read.next().await {
-                        if let Ok(Message::Text(s)) = r {
-                            if let Ok(f) = serde_json::from_str::<FrameJson>(&s) {
-                                // log::debug!("got event from websocket! {:#?}", &f);
-                                // log::info!("current frame: {:#?}", *frame);
-
-                                // let new_frame = frame.push_front(f);
-                                // log::info!("new frame: {:#?}", new_frame.clone());
-
-                                frame.set(*frame);
-
-                                // log::info!("new frame state: {:#?}", *frame);
-                            }
-                        }
-                    }
-                });
-
-                || ()
-            },
-            (),
-        );
-    }
+        use_effect_once(move || {
+            // let frame = frame.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                frame.set(FrameModel::from_request().await);
+            });
+            || ()
+        })
+    };
 
     html! {
         <>
