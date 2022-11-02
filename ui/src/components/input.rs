575b0195b91a18f4cfce9af1a47f89c9dd0e97b0
Joshua Lung <jo.kil.plays@gmail.com>
2022-11-02 18:35:38 +0100

    I forgor 💀

--- ui/src/components/input.rs
+++ ui/src/components/input.rs
@@ -1,119 +1,62 @@
-use anyhow;
 use clone_all::clone_all;
 use common::frame::FrameJson;
-use gloo::events::EventListener;
 
-use web_sys::Event;
+use web_sys::{Event, FocusEvent};
 use yew::{
-    function_component, html, use_effect, use_state, Callback, KeyboardEvent, Properties,
-    UseStateHandle,
+    function_component, html, use_state, Callback, KeyboardEvent, Properties, UseStateHandle,
 };
 
-use crate::util::{get_html_element_by_id, value_from_event};
+use yew_set_state_onchange::set_state;
 
 #[function_component(Input)]
 pub fn frame_input(props: &InputProps) -> Html {
     let text: UseStateHandle<String> = use_state(|| "".to_string());
     let subtext: UseStateHandle<String> = use_state(|| "".to_string());
-    let is_shown: UseStateHandle<bool> = use_state(|| false);
 
     let reset = {
-        clone_all!(text, subtext, is_shown);
+        clone_all!(text, subtext);
         move || {
             text.set("".to_string());
             subtext.set("".to_string());
-            is_shown.set(false);
         }
     };
 
-    use_effect({
-        clone_all!(is_shown, text);
-        move || {
-            let document = gloo::utils::document();
-            let listener = EventListener::new(&document, "keydown", move |_| {
-                if !*is_shown {
-                    is_shown.set(true);
-                    text.set("".to_string());
+    let onsubmit = {
+        clone_all!(text, subtext);
+        let submit_cb = props.submit.clone();
+        move |e: FocusEvent| {
+            e.prevent_default();
 
-                    match get_html_element_by_id("text").map(|el| el.focus()) {
-                        Ok(Ok(_)) => (),
-                        Ok(Err(e)) => log::error!("error focusing on text: {:#?}", e),
-                        Err(e) => log::error!("error getting text element: {:#?}", e),
-                    };
-                }
-            });
-            || drop(listener)
+            let text = Some((*text).clone()).filter(|x| !x.is_empty());
+            let st = Some((*subtext).clone()).filter(|x| !x.is_empty());
+
+            if text.is_none() && st.is_none() {
+                return;
+            } else if text.is_none() && st.is_some() {
+                submit_cb.emit(FrameJson {
+                    text: st.unwrap(),
+                    subtext: None,
+                });
+            } else {
+                submit_cb.emit(FrameJson {
+                    text: text.unwrap(),
+                    subtext: st,
+                });
+            }
+
+            reset();
         }
-    });
+    };
 
     html! {
         <div class={"inputContainer"}>
-            <input
-                class={"input"}
-                id={"text"}
-                onchange={
-                    clone_all!(text);
-                    move |e: Event| {
-                        if let Ok(event) = value_from_event(e) {
-                            text.set(event.to_string());
-                        }
-                    }
-                }
-                onkeydown={
-                    clone_all!(is_shown);
-                    move |e| {
-                        if *is_shown && is_enter(&e) {
-                            if let Err(e) = || -> anyhow::Result<()> {
-                                get_html_element_by_id("text")?.focus().map_err(|_| anyhow::anyhow!("could not focus"))?;
-                                Ok(())
-                            }() {
-                                log::error!("error focusing on text: {:#?}", e);
-                            }
-                        }
-                    }
-                }
-                placeholder={"Text"}
-                value={(*text).clone()}
-                style={if *is_shown {"visibility: visible;"} else {"visibility: hidden;"}}
-            />
-            <input
-                class={"input"}
-                id={"subtext"}
-                onchange={
-                    clone_all!(subtext);
-                    move |e: Event| {
-                        if let Ok(event) = value_from_event(e) {
-                            subtext.set(event.to_string());
-                            log::info!("subtext: {}", *subtext);
-                            // log::info!("{}", e.to_string());
-                        }
-                    }
-                }
-
-                onkeydown={
-                    clone_all!(is_shown, text, subtext);
-                    let submit_cb = props.submit.clone();
-
-                    move |e| {
-                        if *is_shown && is_enter(&e) {
-                            let st = Some((*subtext).clone()).filter(|x| !x.is_empty());
-                            log::info!("{:#?}", &st);
-                            submit_cb.emit(FrameJson { text: (*text).clone(), subtext: st});
-                            reset();
-                        }
-                    }
-                }
-
-                placeholder={"Optional Subtext"}
-                value={(*subtext).clone()}
-                style={if *is_shown {"visibility: visible;"} else {"visibility: hidden;"}}
-            />
+            <form onsubmit={onsubmit}>
+                <input class={"input"} id={"text"} type="text" placeholder={"Text"} value={(*text).clone()} onchange={set_state!(text)}/>
+                <input class={"input"} id={"subtext"} type="text" placeholder={"(Optional Subtext)"} value={(*subtext).clone()} onchange={set_state!(subtext)}/>
+                <input class={"submit"} type="submit" />
+            </form>
         </div>
     }
-}
-
-fn is_enter(e: &KeyboardEvent) -> bool {
-    e.key() == "Enter"
 }
 
 #[derive(Properties, PartialEq)]
