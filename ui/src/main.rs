89027dde5e2a72043817313269aa0220a6a6e66d
Joshua Lung <jo.kil.plays@gmail.com>
2022-11-03 10:26:11 +0100

    ui rework start

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,8 +1,7 @@
-use ui::pages::app::App;
-// use ui::pages::ws_test::chat::Chat;
+use ui::pages::{app::App, ui_rework::UiRework};
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<UiRework>();
     // yew::start_app::<App>();
-    yew::start_app::<App>();
 }
---
+++ ui/src/pages/ui_rework.rs
@@ -0,0 +1,10 @@
+use yew::prelude::*;
+
+#[function_component(UiRework)]
+pub fn uitest() -> Html {
+    html! {
+        <div>
+            <h1>{"Hello World!"}</h1>
+        </div>
+    }
+}
