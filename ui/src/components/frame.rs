49a6cd3901fa411a6bcb954bff0cac49352e538c
Joshua Lung <jo.kil.plays@gmail.com>
2022-11-19 17:42:12 +0100

    color & some scaling

--- ui/src/components/frame.rs
+++ ui/src/components/frame.rs
@@ -19,7 +19,7 @@ pub fn frame(props: &FrameProps) -> Html {
         <AspectContent ratio={1.61803398875}>
             <div
                 class={"w-full h-full border flex flex-col items-center justify-around"}
-                style={format!("padding: {}%; border: 1px solid rgba(255, 255, 255, {})", s * 4., sf(0.9))}
+                style={format!("padding: {}%; border: 1px solid rgba(200, 200, 200, {})", s * 4., sf(0.85))}
             >
                     {
                         match props.depth > props.frames.len() {
@@ -28,43 +28,11 @@ pub fn frame(props: &FrameProps) -> Html {
                         }
                     }
                 <div class="flex flex-col font-serif text-center">
-                    <div style={format!("font-size: {}vw", sf(0.8)*3.)}>{current.text}</div>
-                    <div style={format!("font-size: {}vw", sf(0.8)*1.5)}><p></p></div>
+                    <div style={format!("font-size: {}vw", sf(0.80)*3.)}>{current.text}</div>
+                    <div style={format!("font-size: {}vw", sf(0.80)*1.5)}><p></p></div>
                 </div>
             </div>
         </AspectContent>
-        // <div
-        //     style={format!("height: {}%; width: {}%", s*100., s*100.)}
-        //     class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between items-center"
-        // >
-        //     <div class="border border-green-400 aspect-golden-w flex-grow max-h-full max-w-full"></div>
-        //     <div style={format!("font-size: {}rem", s*2.)} class="flex flex-col font-serif">
-        //         <div>{"What?"}</div>
-        //         <div>{"How?"}</div>
-        //     </div>
-        // </div>
-        // <div class="frameContainer" style={format!("height: {}%; width: {}%", inner_scale*100., inner_scale*100.)} depth={props.depth.to_string()}>
-        //     <div class="frameBorder" style={format!("border: 1px solid rgba(255, 255, 255, {})", sf(0.85))}>
-        //         {match props.frames.get(props.depth as usize + 1) {
-        //             Some(_) => html! {
-        //                 <Frame frames={props.frames.clone()} depth={props.depth + 1} />
-        //             },
-        //             None => html! {},
-        //         }}
-        //     </div>
-
-
-        //     <p class="text" style={format!("font-size: {}vw", 4.0 * scaling_factor)}>{&current.text}</p>
-
-        //     {match &current.subtext
-        //         {
-        //         Some(subtext) => html! {
-        //             // <h2 class="text">{subtext}</h2>
-        //             <p class="text" style={format!("font-size: {}vw", 1.0 * scaling_factor)}>{subtext}</p>
-        //         },
-        //         None => html! {},
-        //     }}
-        // </div>
     )
 }
 
--- ui/src/pages/index/landing.rs
+++ ui/src/pages/index/landing.rs
@@ -7,16 +7,16 @@ pub fn landing() -> Html {
     let context = use_context::<AppContext>().expect("Failed to get context");
 
     html! {
-    <div class="flex h-screen snap-center flex-col justify-between bg-black text-white md:flex-row items-center">
+    <div class="flex h-screen snap-center flex-col justify-between bg-black md:flex-row items-center">
         // <div class="absolute top-0 left-0 min-w-1/2 bg-slate-600">
         //     <Ad slot="4973753444"/>
         // </div>
-        <div class="bg-white h-28 md:w-28 w-full md:h-full"></div>
+        <div class="bg-slate-500 h-28 md:w-28 w-full md:h-full"></div>
         <div class="flex flex-grow flex-col justify-around p-7 md:h-full md:w-auto w-full h-auto">
             <Frame frames={context.frames.clone()} depth=0/>
             <Input />
         </div>
-        <div class="hidden h-28 bg-white md:block md:h-full md:w-28"></div>
+        <div class="hidden h-28 bg-slate-500 md:block md:h-full md:w-28"></div>
     </div>
     }
 }
